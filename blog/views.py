f4857e039194a2bd0af7da6ae600e27d07c4d619
digi0ps <sriru1998@gmail.com>
2017-03-11 23:42:03 +0530

    Tweaked media queries.

--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,8 @@ import markdown2
 
 def blog_index_view(request):
 	all_articles = article.objects.all()
-	for x in all_articles:
-		x.content = markdown2.markdown(x.content, extras=["tables", "cuddled-lists"])
+	for article in all_articles:
+		article.content = markdown2.markdown(article.content, extras=["tables", "cuddled-lists"])
 	return render(request, 'blog/index.html', {"articles": all_articles})
 
 
