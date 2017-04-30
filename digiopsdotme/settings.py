f319ac153e891a425ea8d286099c8d9a1412af21
digi0ps <sriru1998@gmail.com>
2017-04-30 13:46:22 +0530

    Use postgres database.

--- digiopsdotme/settings.py
+++ digiopsdotme/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'digiopsdotme.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'digiopsdotme',
+        'USER': 'digi0ps',
+        'PASSWORD': 'digi0ps1234',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
